cceec1b5c4f4dc6e19c6b84db76ec86602a14335
EthanWilliams27 <ethanwilliams092703@gmail.com>
2025-02-24 16:08:42 -0600

    Added code to balanced brackets using stack, to check if the brackets or balanced, if they are we return yes, otherwise we return no.

--- BalancedBrackets.java
+++ BalancedBrackets.java
@@ -1,2 +1,25 @@
+import java.util.Stack;
 public class BalancedBrackets {
+    public static String isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return "No";
+                }
+                char top = stack.pop();
+                if ((c == '}' && top != '{') || (c == ']' && top != '[') || (c == ')' && top != '(')) {
+                    return "No";
+                }
+            }
+        }
+
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+    public static void main(String[] args) {
+        String s = "{}[)[]";
+        System.out.println(isBalanced(s));
+    }
 }
