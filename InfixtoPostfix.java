0b3d4ffcc458c912248e00f8f850d3a562035bdf
EthanWilliams27 <ethanwilliams092703@gmail.com>
2025-02-24 16:18:16 -0600

    Added InfixtoPostfix class

---
+++ InfixtoPostfix.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+public class InfixtoPostfix {
+
+    public static String infixToPostfix(String s) {
+        String result = "";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                result += c;
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result += stack.pop();
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result += stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+
+    private static int precedence(char c) {
+        if (c == '+' || c == '-') {
+            return 1;
+        } else if (c == '*' || c == '/') {
+            return 2;
+        } else if (c == '^') {
+            return 3;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "a+b*(c^d-e)^(f+g*h)-i";
+        System.out.println(infixToPostfix(s));
+    }
+}
