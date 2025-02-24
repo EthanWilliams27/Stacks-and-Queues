69ad3a2325d3ec698726d06c231e5c8a086259c1
EthanWilliams27 <ethanwilliams092703@gmail.com>
2025-02-24 16:11:54 -0600

    Added code to DecodeString class, which takes a string such as 3[a]2[b] and decodes it to where it would be aaabb.

---
+++ DecodeString.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class DecodeString {
+
+    public static String decodeString(String s) {
+        Stack<Integer> counts = new Stack<>();
+        Stack<String> result = new Stack<>();
+        String currentString = "";
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                count = count * 10 + (c - '0');
+            } else if (c == '[') {
+                counts.push(count);
+                result.push(currentString);
+                currentString = "";
+                count = 0;
+            } else if (c == ']') {
+                String decodedString = currentString;
+                currentString = result.pop();
+                int repeatCount = counts.pop();
+
+                for (int i = 0; i < repeatCount; i++) {
+                    currentString += decodedString;
+                }
+            } else {
+                currentString += c;
+            }
+        }
+
+        return currentString;
+    }
+
+    public static void main(String[] args) {
+        String s = "3[a2[c]]";
+        System.out.println(decodeString(s));
+    }
+}
